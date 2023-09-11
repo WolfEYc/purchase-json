2c25be7e3f781347589b45851c39ae825f5bcd25
WolfEYc <isaac.wolf@live.com>
2023-09-11 13:19:00 -0500

    booty

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ async fn main() -> Result<()> {
     let all_endpoins = (AccountsApi, PurchasesApi);
     let api_service = OpenApiService::new(all_endpoins, "Purchase Api", "1.0");
     let ui = api_service.openapi_explorer();
-    let app = Route::new().nest("/", api_service).nest("/docs", ui).with(Cors::new());
+    let app = Route::new()
+		.nest("/", api_service)
+		.nest("/docs", ui)
+		.with(Cors::new());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = TcpListener::bind(addr);
