80ba2bd04577ac99ac713d0a520cdd4bbbe6be66
WolfEYc <isaac.wolf@live.com>
2023-09-13 13:05:13 -0500

    seperate purchase date and time

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::net::SocketAddr;
-use poem::{listener::TcpListener, Route, Server, EndpointExt, middleware::{Cors, SetHeader}, http::{header::CACHE_CONTROL, HeaderValue}};
-use poem_openapi::OpenApiService;
 use color_eyre::Result;
-use purchase_json::{state::create_appstate, accounts::AccountsApi, purchases::PurchasesApi};
+use poem::{listener::TcpListener, middleware::Cors, EndpointExt, Route, Server};
+use poem_openapi::OpenApiService;
+use purchase_json::{accounts::AccountsApi, purchases::PurchasesApi, state::create_appstate};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -11,17 +11,15 @@ async fn main() -> Result<()> {
     let api_service = OpenApiService::new(all_endpoins, "Purchase Api", "1.0");
     let ui = api_service.openapi_explorer();
     let app = Route::new()
-		.nest("/", api_service)
-		.nest("/docs", ui)
-		.with(SetHeader::new().overriding(CACHE_CONTROL, HeaderValue::from_static("public, max-age=3600")))
-		.with(Cors::new());
+        .nest("/", api_service)
+        .nest("/docs", ui)
+        //.with(SetHeader::new().overriding(CACHE_CONTROL, HeaderValue::from_static("public, max-age=3600")))
+        .with(Cors::new());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = TcpListener::bind(addr);
 
-    Server::new(listener)
-        .run(app)
-        .await?;
+    Server::new(listener).run(app).await?;
 
     Ok(())
 }
