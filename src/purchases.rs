4c3cdc99b314a8ea1c1a6971a11e2ef943e8796b
WolfEYc <isaac.wolf@live.com>
2023-09-12 20:03:43 -0500

    paginate on 30

--- src/purchases.rs
+++ src/purchases.rs
@@ -1,8 +1,14 @@
-use poem_openapi::{OpenApi, payload::Json, Object};
-use serde::Deserialize;
-use sqlx::{types::{chrono::{NaiveDate, NaiveDateTime}, BigDecimal}, FromRow, QueryBuilder, Postgres, Execute};
-use poem::{Result, http::StatusCode, Request};
 use crate::{state::state, PAGE_SIZE};
+use poem::{http::StatusCode, Request, Result};
+use poem_openapi::{payload::Json, Object, OpenApi};
+use serde::Deserialize;
+use sqlx::{
+    types::{
+        chrono::{NaiveDate, NaiveDateTime},
+        BigDecimal,
+    },
+    Execute, FromRow, Postgres, QueryBuilder,
+};
 use tracing::info;
 
 #[derive(Debug)]
@@ -12,7 +18,8 @@ impl PurchasesApi {
     #[oai(path = "/purchase", method = "get")]
     async fn read(&self, req: &Request) -> Result<Json<Vec<Purchase>>> {
         let filter = req.params::<PurchaseFilter>()?;
-        let purchases = read(filter).await
+        let purchases = read(filter)
+            .await
             .map_err(|e| poem::Error::from_string(e.to_string(), StatusCode::BAD_REQUEST))?;
 
         Ok(Json(purchases))
@@ -37,13 +44,13 @@ impl From<PurchaseRow> for Purchase {
         Purchase {
             account_number: value.account_number,
             purchase_datetime: value.purchase_datetime,
-            purchase_amount: value.purchase_amount.to_string(),
-            post_date: value.post_date, 
-            purchase_number: value.purchase_number, 
-            merchant_number: value.merchant_number, 
-            merchant_name: value.merchant_name, 
-            merchant_state: value.merchant_state, 
-            merchant_category_code: value.merchant_category_code 
+            purchase_amount: value.purchase_amount.with_scale(2).to_string(),
+            post_date: value.post_date,
+            purchase_number: value.purchase_number,
+            merchant_number: value.merchant_number,
+            merchant_name: value.merchant_name,
+            merchant_state: value.merchant_state,
+            merchant_category_code: value.merchant_category_code,
         }
     }
 }
@@ -73,7 +80,6 @@ pub struct PurchaseFilter {
     pub merchant_state: Option<String>,
     pub merchant_category_code: Option<i16>,
     pub page: i64,
-	pub lookahead: i64
 }
 
 pub async fn read(filter: PurchaseFilter) -> color_eyre::Result<Vec<Purchase>> {
@@ -81,24 +87,38 @@ pub async fn read(filter: PurchaseFilter) -> color_eyre::Result<Vec<Purchase>> {
 
     let mut seperated = query.separated(" AND ");
     if let Some(account_number) = &filter.account_number {
-        seperated.push("account_number = ").push_bind_unseparated(account_number);
+        seperated
+            .push("account_number = ")
+            .push_bind_unseparated(account_number);
     }
     if let Some(purchase_number) = &filter.purchase_number {
-        seperated.push("purchase_number = ").push_bind_unseparated(purchase_number);
+        seperated
+            .push("purchase_number = ")
+            .push_bind_unseparated(purchase_number);
     }
     if let Some(merchant_state) = &filter.merchant_state {
-        seperated.push("merchant_state = ").push_bind_unseparated(merchant_state);
+        seperated
+            .push("merchant_state = ")
+            .push_bind_unseparated(merchant_state);
     }
     if let Some(merchant_category_code) = &filter.merchant_category_code {
-        seperated.push("merchant_category_code = ").push_bind_unseparated(merchant_category_code);
+        seperated
+            .push("merchant_category_code = ")
+            .push_bind_unseparated(merchant_category_code);
     }
     if let Some(merchant_number) = &filter.merchant_number {
-        seperated.push("LOWER(merchant_number) LIKE '%' || ").push_bind_unseparated(merchant_number.to_lowercase()).push_unseparated(" || '%'");
+        seperated
+            .push("LOWER(merchant_number) LIKE '%' || ")
+            .push_bind_unseparated(merchant_number.to_lowercase())
+            .push_unseparated(" || '%'");
     }
     if let Some(merchant_name) = &filter.merchant_name {
-        seperated.push("LOWER(merchant_name) LIKE '%' || ").push_bind_unseparated(merchant_name.to_lowercase()).push_unseparated(" || '%'");
+        seperated
+            .push("LOWER(merchant_name) LIKE '%' || ")
+            .push_bind_unseparated(merchant_name.to_lowercase())
+            .push_unseparated(" || '%'");
     }
-    
+
     if query.sql().ends_with("WHERE ") {
         query = QueryBuilder::new("SELECT * FROM purchase");
     }
@@ -106,24 +126,37 @@ pub async fn read(filter: PurchaseFilter) -> color_eyre::Result<Vec<Purchase>> {
     query.push(" ORDER BY ");
     let mut seperated = query.separated(", ");
     if let Some(purchase_datetime) = &filter.purchase_datetime {
-        seperated.push("ABS(EXTRACT(EPOCH FROM (purchase_datetime - ").push_bind_unseparated(purchase_datetime).push_unseparated("))) ASC");
+        seperated
+            .push("ABS(EXTRACT(EPOCH FROM (purchase_datetime - ")
+            .push_bind_unseparated(purchase_datetime)
+            .push_unseparated("))) ASC");
     }
     if let Some(post_date) = &filter.post_date {
-        seperated.push("ABS(post_date - ").push_bind_unseparated(post_date).push_unseparated(") ASC");
+        seperated
+            .push("ABS(post_date - ")
+            .push_bind_unseparated(post_date)
+            .push_unseparated(") ASC");
     }
     if let Some(purchase_amount) = &filter.purchase_amount {
-        seperated.push("ABS(purchase_amount - ").push_bind_unseparated(purchase_amount).push_unseparated(") ASC");
+        seperated
+            .push("ABS(purchase_amount - ")
+            .push_bind_unseparated(purchase_amount)
+            .push_unseparated(") ASC");
     }
-    if query.sql().ends_with("ORDER BY ")  {
+    if query.sql().ends_with("ORDER BY ") {
         query.push("purchase_datetime DESC");
     }
 
-    query.push(format!(" LIMIT {} OFFSET ", (filter.lookahead + 1) * PAGE_SIZE)).push_bind(filter.page * PAGE_SIZE);
+    query
+        .push(format!(" LIMIT {} OFFSET ", PAGE_SIZE))
+        .push_bind(filter.page * PAGE_SIZE);
 
     let query = query.build_query_as::<PurchaseRow>();
     info!("{:?}", query.sql());
 
-    Ok(query.fetch_all(&state().db).await?
+    Ok(query
+        .fetch_all(&state().db)
+        .await?
         .into_iter()
         .map(Purchase::from)
         .collect())
